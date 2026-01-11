5005c5d40187824a77f4b541ad7493a9a61f2cb7
choiguen <daindj0@gmail.com>
2026-01-11 19:26:17 +0900

    Chore: 주석 제거

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,5 +1,20 @@
-def test_smoke() -> None:
-    assert True
-    """
-dd
-"""
+from fastapi.testclient import TestClient
+from app.main import app
+from app.core.config import settings
+
+client = TestClient(app)
+
+def test_root_when_db_host_exists(monkeypatch):
+    monkeypatch.setattr(settings, "DB_HOST", "localhost")
+
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert resp.json() == "success: localhost"
+
+def test_root_when_db_host_missing(monkeypatch):
+    monkeypatch.setattr(settings, "DB_HOST", "")
+
+    resp = client.get("/")
+    assert resp.status_code == 200
+
+    assert resp.json() == ["false"]

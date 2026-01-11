54f2492e899dc5590c0e42e9d3bd750c95b6390c
choiguen <daindj0@gmail.com>
2026-01-12 01:17:31 +0900

    Chore: test 파일 임시 삭제

--- tests/test_smoke.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-
-def test_root_returns_200():
-    client = TestClient(app)
-    res = client.get("/")
-
-    assert res.status_code == 200

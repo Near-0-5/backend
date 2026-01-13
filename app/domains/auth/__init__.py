89ea73dde51641c5e1a9d59a673b13e48841b16e
ji-min0 <k.jimin2002@gmail.com>
2026-01-13 11:36:10 +0900

    Style: 코드 포매팅 및 mypy 대응

--- app/domains/auth/__init__.py
+++ app/domains/auth/__init__.py
@@ -1,4 +1,5 @@
 """
 app/domains/auth/
-- 소셜 로그인/회원가입(카카오/네이버) 도메인. 외부 OAuth는 integrations/*를 호출하고, 우리 서비스 User upsert 및 JWT 발급을 수행.
+- 소셜 로그인/회원가입(카카오/네이버) 도메인.
+- 외부 OAuth는 integrations/*를 호출하고, 우리 서비스 User upsert 및 JWT 발급을 수행.
 """

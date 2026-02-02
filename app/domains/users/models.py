18fb3c331d217e491a047e921aeb47a0b9a3de1a
ji-min0 <k.jimin2002@gmail.com>
2026-02-02 19:21:35 +0900

    [NEAR-119] Refactor: Concert 모델 분리에 따른 임포트 경로 수정

--- app/domains/users/models.py
+++ app/domains/users/models.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 
 from tortoise import fields, models
 
-from app.domains.streams.models import CategoryType
+from app.domains.concerts.models import CategoryType
 
 if TYPE_CHECKING:
     from app.domains.artists.models import Artist

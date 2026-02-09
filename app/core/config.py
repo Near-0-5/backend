7355b2dede228dde0201ab830b4201065c6a4b22
Jamily <223883617+JaMiLy-max@users.noreply.github.com>
2026-02-09 12:10:02 +0900

    [NEAR-146] Chore: 주요 설정 및 의존성 파일 특정 시점으로 원복
    서버리스 시도 전으로 원복

--- app/core/config.py
+++ app/core/config.py
@@ -49,7 +49,6 @@ class Settings(BaseSettings):
     # REDIS 설정
     REDIS_HOST: str
     REDIS_PORT: str
-    REDIS_PROTOCOL: str = "redis"
 
     AUTO_SCHEMA: str = "0"  # 개발 초기에만 1로 켜서 generate_schemas를 쓰는 경우에 사용
 
@@ -96,17 +95,12 @@ class Settings(BaseSettings):
     @computed_field  # type: ignore[prop-decorator]
     @property
     def DATABASE_URL(self) -> str:
-        url = f"{self.DB_SCHEME}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-        # DB_HOST 주소에 'amazonaws.com'이 포함되어 있다면 (즉, RDS라면) SSL 옵션 추가
-        if "amazonaws.com" in self.DB_HOST:
-            url += "?sslmode=require"
-        return url
+        return f"{self.DB_SCHEME}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
     @computed_field  # type: ignore[prop-decorator]
     @property
     def REDIS_URL(self) -> str:
-        return f"{self.REDIS_PROTOCOL}://{self.REDIS_HOST}:{self.REDIS_PORT}/0"
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/0"
 
     @property
     def IVS_PLAYBACK_PRIVATE_KEY(self) -> str:
